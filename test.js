f277b250acab8d66a960781788ea6fafb5ad9ce1
Mats Selen <mats@illinois.edu>
2021-01-06 13:58:26 -0600

    file up/download tests

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 // some test code
 function saveToFile() {
-    var dataBlob = new Blob([document.getElementById("dataBoxTx").value], {type: "text/plain;charset=utf-8"});
+    var dataBlob = new Blob([document.getElementById("dataBoxRx").value], {type: "text/plain;charset=utf-8"});
     //var dataBlob = new Blob(rawData);
     downloadData.href = window.URL.createObjectURL(dataBlob);
     downloadData.download = "IOLab-data-test.txt";
 }
 
 function parseFromFile(fileContents) {
-    document.getElementById("dataBoxTx").value = fileContents;
+    document.getElementById("dataBoxRx").value = fileContents;
 }
 
 async function readInputFile() {
