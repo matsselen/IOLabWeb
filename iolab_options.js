829d5f11e02953e5d6640600d0cdb8da7b02de26
Mats Selen <mats@illinois.edu>
2021-01-28 09:01:58 -0600

    save
    change default config

--- iolab_options.js
+++ iolab_options.js
@@ -168,7 +168,7 @@ function buildConfigPicker() {
 
   //configPicker.selectedIndex = 17; // default to "kitchen sink"
   //configPicker.selectedIndex = 13; // default to accelerometer
-  configPicker.selectedIndex = 13; // high speed orientation
+  configPicker.selectedIndex = 12; // high speed orientation
   current_config = configPicker.options[configPicker.selectedIndex].value;
   current_config_code = iolabConfig.fixedConfigurations[configPicker.selectedIndex]["code"];
   document.getElementById('config-picker').style.visibility = "hidden";
