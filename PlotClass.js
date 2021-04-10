634771c4a60033ccccc97cb6eba2c204f015123f
Mats Selen <mats@illinois.edu>
2021-04-10 07:12:08 -0500

    Update PlotClass.js

--- PlotClass.js
+++ PlotClass.js
@@ -768,11 +768,12 @@ class PlotIOLab {
                 date.toTimeString().substr(6, 2) + "_" +
                 "sens_" + plotThis.sensorNum.toString() + ".csv";
 
-            setTimeout(async function () {
-                // save the data as a local download
-                plotThis.aCSV.href = window.URL.createObjectURL(dataBlob), { type: "text/csv;charset=utf-8" };
-                plotThis.aCSV.download = fName;
-            }, 100);
+            // save the data as a local download
+            plotThis.aCSV.href = window.URL.createObjectURL(dataBlob), { type: "text/csv;charset=utf-8" };
+            plotThis.aCSV.download = fName;
+            
+            // setTimeout(async function () {
+            // }, 200);
 
         }
 
