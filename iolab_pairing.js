fc88ce21eed206a20bc217bf3e400a16438af049
Mats Selen <mats@illinois.edu>
2021-05-20 10:32:36 -0500

    Update iolab_pairing.js

--- iolab_pairing.js
+++ iolab_pairing.js
@@ -13,3 +13,22 @@ function openPairModal() {
 function endPair() {
 
 }
+
+function sendPair() {
+
+}
+
+function sendUnpair() {
+  let status = 0; // 0=unpair, 1=pair 
+  let id0 = remote1ID & 0xFF;
+  let id1 = (remote1ID>>8) & 0xFF;
+  let id2 = (remote1ID>>16) & 0xFF;
+
+  byteArray = getCommandRecord(1, [status,id2,id1,id0], "getPacketConfig");
+  console.log(byteArray);
+  await sendRecord(byteArray);
+}
+
+function sendFind() {
+
+}
