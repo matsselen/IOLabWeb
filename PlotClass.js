64f4a1f9a3a7d5a8ebf2f2cd58b5e995e17c8282
Mats Selen <mats@illinois.edu>
2021-07-02 10:16:29 -0500

    Update PlotClass.js

--- PlotClass.js
+++ PlotClass.js
@@ -1533,8 +1533,8 @@ class PlotIOLab {
         ctx.font = "14px Arial";
         let xLabel = "t (s)";
         let xLabelWidth = ctx.measureText(xLabel).width;
-        ctx.fillText(xLabel, this.baseElement.width - xLabelWidth - 5, this.baseElement.height );
-        //ctx.fillText(xLabel, (this.baseElement.width - xLabelWidth)/2, this.baseElement.height );
+        //ctx.fillText(xLabel, this.baseElement.width - xLabelWidth - 5, this.baseElement.height );
+        ctx.fillText(xLabel, (this.baseElement.width - xLabelWidth)/2, this.baseElement.height );
         ctx.font = "12px Arial";
 
         // y-axis: pick the starting data value, interval, and precision based on viewport
